8b775ba621f8e06d927261d0526661240823ab87
shadab75 <80451213+shadab75@users.noreply.github.com>
2024-09-27 14:45:37 +0330

    finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./pages/Home";
+import Header from "./components/Header";
+import store from "./redux/store";
+import {Provider} from "react-redux"
+import Products from "./pages/Products";
+import ShoppingCart from "./pages/ShoppingCart";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  return(
+      <BrowserRouter>
+          <Provider store={store}>
+          <Header/>
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/products" element={<Products/>}></Route>
+          <Route path="/cart" element={<ShoppingCart/>}></Route>
+        </Routes>
+         </Provider>
+      </BrowserRouter>
+  )
+
 }
 
 export default App;
---
+++ src/redux/rootReducer.js
@@ -0,0 +1,9 @@
+import {combineReducers} from "redux";
+import productReducer from "./products/reducer";
+import cartReducer from "./cart/reducer";
+const rootReducer = combineReducers({
+    product:productReducer,
+    shoppingCart:cartReducer
+})
+
+export default rootReducer;
